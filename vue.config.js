b188ea32883a2441802f3924326c2a03e82641c9
pugetoken <pugetoken@gmail.com>
2021-05-01 22:37:46 +0800

    second deploy

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const webpack = require('webpack');
 
 module.exports = {
-  publicPath: {process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'production'
     ? '/websitedemo/'
     : '/'
-  },
+  ,
 
   configureWebpack: {
     // Set up all the aliases we use in our app.
